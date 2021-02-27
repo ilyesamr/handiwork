3948a93351a97db0ef100f8a110dc62a2928e74c
20014086 <ilyes.ameurameur@adeo.com>
2021-02-27 15:56:55 +0100

    server setup

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const config = require('./config/config')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+console.log("hello from backend")
+app.listen(config.port)
+console.log(`Server started on port ${config.port}`)
