998430b660d5eb9cc77bba204462ed6c44580a90
20014086 <ilyes.ameurameur@adeo.com>
2021-04-03 19:58:42 +0200

    backend url added

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
 export default () => {
-    return axios.create({
-        baseURL: 'http://localhost:8081/'
-    })
+ if (process.env.NODE_ENV === 'production') {
+        return axios.create({
+            baseURL: 'https://handi-backend.herokuapp.com/'
+        })
+    } else if (process.env.NODE_ENV === 'development') {
+        return axios.create({
+            baseURL: 'http://localhost:8081/'
+        })
+    }
 }
